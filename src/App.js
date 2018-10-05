6a4eac68c7da63ef9ac76ee4abbd4216d6cea5cd
seanfmcd <seanfmcd01@gmail.com>
2018-10-05 01:33:10 +0100

    add eslint and prettier config, lint all files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Bob" age="23"/>
-        <Person name="Jill" age="31"/>
-        <Person name="Sophie" age="25"/>
+        <Person name="Bob" age="23" />
+        <Person name="Jill" age="31" />
+        <Person name="Sophie" age="25" />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const person = (props) => {
-  return <p>I'm {props.name} and I am {props.age} years old</p>
-}
+const person = props => {
+  return (
+    <p>
+      I'm {props.name} and I am {props.age} years old
+    </p>
+  );
+};
 
-export default person
+export default person;
